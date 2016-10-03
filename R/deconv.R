4ae32af0d87d210fe3aa59ebcb4e42ec6ff5bfd4
Greg Hunt <hunt.gregory.james@gmail.com>
2016-10-03 12:11:19 -0400

    Initial commit.

---
+++ R/deconv.R
@@ -0,0 +1,24 @@
+# --> Y
+# A single DNA microarray to deconvolve
+# --> marker_probes
+# list of list of vectors of probes. Level 1 is cell type, level two is genes.
+# --> thetas
+# same structure as marker_probes but contains theta estimates for probes.
+# --> gamma
+# scalar estimate of gamma
+deconv = function(Y, marker_probes, thetas, gamma){
+    K = length(marker_probes)
+    
+    contrib_est = function(i){
+        Y_i = lapply(marker_probes[[i]],function(x){Y[,x]})
+        theta_adj = mapply('-',Y_i,thetas[[i]],SIMPLIFY=FALSE)
+        ests = lapply(theta_adj,function(x){mean(2^(x/gamma))})
+        amt = Reduce("+",ests)/length(ests)
+        return(amt)
+    }
+
+    contribs = sapply(1:K,contrib_est)
+    phats = contribs / sum(contribs)
+
+    return(phats)
+}
